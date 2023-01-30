016343375d685d40cf593271fcfd803239f1a9f0
Nurseiit Sharip <nurseit.js@gmail.com>
2023-01-30 17:42:54 +0600

    feat: add skeleton for payment & delivery text

--- src/components/PaymentDelivery/index.tsx
+++ src/components/PaymentDelivery/index.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Skeleton, Typography } from "@mui/material";
 
 import Card from "./Card";
 
@@ -8,20 +8,52 @@ import Car from "assets/svg/car.svg";
 import ReactMarkdown from "react-markdown";
 
 import { useAboutInfo } from "hooks/useAboutInfo";
+import classes from "components/About/index.module.css";
+
+const TypographySkeleton = () => {
+  return (
+    <>
+      <Skeleton width="100%">
+        <Typography className={classes.skeletonText}>.</Typography>
+      </Skeleton>
+      <Skeleton width="100%">
+        <Typography className={classes.skeletonText}>.</Typography>
+      </Skeleton>
+      <Skeleton width="30%">
+        <Typography className={classes.skeletonText}>.</Typography>
+      </Skeleton>
+      <Skeleton width="100%">
+        <Typography className={classes.skeletonText}>.</Typography>
+      </Skeleton>
+      <Skeleton width="100%">
+        <Typography className={classes.skeletonText}>.</Typography>
+      </Skeleton>
+      <Skeleton width="30%">
+        <Typography className={classes.skeletonText}>.</Typography>
+      </Skeleton>
+    </>
+  );
+};
 
 const PaymentDelivery = () => {
   const { about } = useAboutInfo();
   return (
     <Grid container>
       <Card icon={Cart} title="Оплата">
-        <Typography>
-          <ReactMarkdown>{about.payment}</ReactMarkdown>
-        </Typography>
+        {about.payment && (
+          <Typography>
+            <ReactMarkdown>{about.payment}</ReactMarkdown>
+          </Typography>
+        )}
+        {!about.payment && <TypographySkeleton />}
       </Card>
       <Card icon={Car} title="Доставка">
-        <Typography>
-          <ReactMarkdown>{about.delivery}</ReactMarkdown>
-        </Typography>
+        {about.delivery && (
+          <Typography>
+            <ReactMarkdown>{about.delivery}</ReactMarkdown>
+          </Typography>
+        )}
+        {!about.delivery && <TypographySkeleton />}
       </Card>
     </Grid>
   );

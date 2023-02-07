62c71c60bd98711eb759f3dad8681d7db1e54da9
Nurseiit Sharip <nurseit.js@gmail.com>
2023-02-08 02:01:18 +0600

    feat: change green api instances

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export const BASE_URL = "https://glacage.onrender.com";
 export const GREEN_API_URL = "https://api.green-api.com";
-export const idInstance = "1101790970";
+export const idInstance = "1101790977";
 export const apiTokenInstance =
-  "a3c8565286484939a555c4be9b5e3c91c4bc63808338457890";
+  "24b73e312a21416ba573a510723e1a515e7a3c5c2a764a3eb3";
 
 export const MAX_PAGE_SIZE = 10000;
 

5e036f8fe446353a5c335a6f4e4a80ce71c733d0
Nurseiit Sharip <nurseit.js@gmail.com>
2023-08-08 19:04:08 +0600

    ci: push dist files

---
+++ dist/assets/index.785ed1b3.js
@@ -0,0 +1,1 @@
+import{a}from"./index.6d5ea256.js";const o=t=>a.get(`/api/products?populate=*&filters[category]=${t}`),r=t=>a.get(`/api/products/${t}?populate=*&pagination[pageSize]=`);export{r as a,o as g};
